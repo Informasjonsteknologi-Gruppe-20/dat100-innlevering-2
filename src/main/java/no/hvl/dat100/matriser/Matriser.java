c1449b10f103516ef7037b810446c1a3a87c759e
Lars <lduinker04@gmail.com>
2025-10-01 11:31:33 +0200

    Oppgave 5 og 6

--- src/main/java/no/hvl/dat100/matriser/Matriser.java
+++ src/main/java/no/hvl/dat100/matriser/Matriser.java
@@ -23,34 +23,50 @@ public class Matriser {
 
 	// c)
 	public static int[][] skaler(int tall, int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("Metoden skaler ikke implementert");
-	
+		int[][] skalert = new int[matrise.length][matrise[0].length];
+		for(int i = 0; i < matrise.length;i++){
+			for(int j = 0; j < matrise[i].length;j++){
+				skalert[i][j] = matrise[i][j] * tall;
+			}
+		}
+		return skalert;
 	}
 
 	// d)
 	public static boolean erLik(int[][] a, int[][] b) {
-
-		// TODO
-		throw new UnsupportedOperationException("Metoden erLik ikke implementert");
-		
+		if(a.length != b.length) return false;
+		for(int i = 0;i < a.length; i++){
+			if(a[i].length != b[i].length) return false;
+			for(int j = 0;j < a[i].length; j++){
+				if(a[i][j] != b[i][j]) return false;
+			}
+		}
+		return true;
 	}
 	
 	// e)
 	public static int[][] speile(int[][] matrise) {
-
-		// TODO
-
-		throw new UnsupportedOperationException("Metoden speile ikke implementert");
-	
+		int[][] speilet = new int[matrise.length][matrise[0].length];
+		for(int i = 0;i < matrise.length;i++){
+			for(int j = 0;j < matrise[i].length;j++){
+				speilet[j][i] = matrise[i][j];
+			}
+		}
+		return speilet;
 	}
 
 	// f)
 	public static int[][] multipliser(int[][] a, int[][] b) {
-
-		// TODO
-		throw new UnsupportedOperationException("Metoden multipliser ikke implementert");
-	
+		int[][] c = new int[a.length][b[0].length];
+		for (int i = 0; i < a.length;i++){
+			for (int j = 0; j < b.length;j++){
+				int sum = 0;
+				for(int k = 0; k < a[i].length;k++){
+					sum += a[i][k] * b[k][j];
+				}
+				c[i][j] = sum;
+			}
+		}
+		return c;
 	}
 }
