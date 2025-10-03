d0dbf09c4bf852395cb4e71b96a9370f6f4d8023
Christian Fanavoll <chfan9733@hvl.no>
2025-10-03 10:05:49 +0200

    Tabeller ferdig, Matriser ferdig.

--- src/main/java/no/hvl/dat100/matriser/Matriser.java
+++ src/main/java/no/hvl/dat100/matriser/Matriser.java
@@ -4,33 +4,67 @@ public class Matriser {
 
 	// a)
 	public static void skrivUt(int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("Metoden skrivUt ikke implementert");
-	}
+		for (int[] rad : matrise) {
+            for (int verdi : rad) {
+                System.out.print(verdi + " ");
+            }
+            System.out.println();
+	    }
+    }
 
 	// b)
 	public static String tilStreng(int[][] matrise) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden tilStreng ikke implementert");
-		
+		String lagStreng = "";
+
+        for (int[] rad : matrise) {
+
+            for (int verdi : rad) {
+                lagStreng += verdi + " ";
+            }
+
+            lagStreng += "\n";
+
+        }
+
+        return lagStreng;
+
 	}
 
 	// c)
 	public static int[][] skaler(int tall, int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("Metoden skaler ikke implementert");
-	
+
+        int rad = matrise.length;
+        int kol = matrise[0].length;
+
+		int[][] skalert = new int[rad][kol];
+
+        for (int i = 0; i < rad; i++) {
+            for (int j = 0; j < kol; j++) {
+                skalert[i][j] = matrise[i][j] * tall;
+            }
+        }
+
+        return skalert;
 	}
 
 	// d)
 	public static boolean erLik(int[][] a, int[][] b) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden erLik ikke implementert");
-		
+       int rad = 0;
+       if (a.length != b.length) return false;
+
+       for (int[] aa : a) {
+            int kol = 0;
+            for (int aaa : aa) {
+                if (aa.length != b[rad].length) return false;
+                if (aaa != b[rad][kol]) return false;
+                kol++;
+            }
+            rad++;
+        }
+
+		return true;
 	}
 	
 	// e)
